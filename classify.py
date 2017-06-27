62bc7e341d5f6f4d25f3dfc00b6c393942083853
vendi <svitlana.vakulenko@gmail.com>
2017-06-27 14:06:38 +0200

    new: training fasttext classifier

--- classify.py
+++ classify.py
@@ -12,6 +12,13 @@ Identifies the tweets containing a job ad
 # Load text processing libraries
 import pandas as pd
 
+import fasttext
+
+
+DATASET_PATH = 'data/all_tweets.csv'
+DATASET_PATH_FT_FORMAT = 'data/all_tweets_ft.csv'
+TEST_DATASET_PATH = 'data/test_tweets_ft.csv'
+
 
 # 1. Load the labeled dataset into a dataframe
 def load_dataset(dataset_path):
@@ -26,7 +33,7 @@ def test_load_dataset():
     print df[0]
 
 
-def convert_dataset(dataset_path, output_path):
+def prepare_dataset(dataset_path, output_path):
     '''
     dataset formatting for fastText classification
     + remove paragraphs with line separators: save one tweet per line
@@ -34,15 +41,23 @@ def convert_dataset(dataset_path, output_path):
     + shuffle samples
     '''
     df = load_dataset(dataset_path)
-    df[0] = df[0].str.replace('\n', '')
+    
+    # preprocessing:
+    # strip new line chars
+    df[1] = df[1].str.replace('\n', '')
+    # lowercase
+    df[1] = df[1].str.lower()
+    # df[0] = str(df[0]).lower()
+    df[0] = '__label__' + df[0].astype(str)
+
     print "Loaded table", df.shape
     # with open(output_path, 'w') as f_out:
     #     for row in df.rows():
     #         f_out.write(row)
     # show class distribution
     # print df.groupby(1).count()
-    false_samples = df.loc[df[1]==False]
-    true_samples = df.loc[df[1]==True]
+    false_samples = df.loc[df[0]=='__label__False']
+    true_samples = df.loc[df[0]=='__label__True']
     print len(false_samples), len(true_samples)
     # downsample false classes
     n_samples_per_class = min(len(false_samples), len(true_samples))
@@ -53,13 +68,35 @@ def convert_dataset(dataset_path, output_path):
     # shuffle
     dataset = dataset.sample(frac=1).reset_index(drop=True)
     print len(dataset)
-    dataset.to_csv(output_path, sep=',', index=False, header=False, columns=[1,0])
+    dataset.to_csv(output_path, sep=',', index=False, header=False)
 
 
-def test_convert_dataset():
-    dataset_path = 'data/my_tweets.csv'
-    output_path = 'data/my_tweets_ft.csv'
-    convert_dataset(dataset_path, output_path)
+def test_prepare_dataset(dataset_path=DATASET_PATH, output_path=DATASET_PATH_FT_FORMAT):
+    prepare_dataset(dataset_path, output_path)
+
+
+def train_fasttext(dataset_path=DATASET_PATH_FT_FORMAT):
+    # training
+    # ./fasttext supervised -input data.train.txt -output model
+    classifier = fasttext.supervised(dataset_path, 'model', epoch=50)
+    classifier.word_ngrams = 5
+    evaluate_classifier(classifier)
+
+
+def evaluate_classifier(classifier):
+    # evaluation
+    # ./fasttext test model.bin test.txt
+    result = classifier.test(TEST_DATASET_PATH)
+    print 'P@1:', result.precision
+    print 'R@1:', result.recall
+    print 'Number of examples:', result.nexamples
+
+
+def explain_classifier():
+    # load model
+    classifier = fasttext.load_model('model.bin', encoding='utf-8')
+    # evaluation
+    evaluate_classifier(classifier)
 
 
 # def preprocess(tweets):
@@ -86,4 +123,6 @@ def test_convert_dataset():
 
 
 if __name__ == '__main__':
-    test_convert_dataset()
+    # test_prepare_dataset()
+    # train_fasttext()
+    explain_classifier()
